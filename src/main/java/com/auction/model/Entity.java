482be33ada476ae9c0514c2b08842021aba6895c
lan-hi <25021842@vnu.edu.vn>
2026-04-08 18:45:07 +0700

    Update Entity.java

---
+++ src/main/java/com/auction/model/Entity.java
@@ -0,0 +1,10 @@
+public abstract class Entity {
+    protected String id;
+    
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+}
