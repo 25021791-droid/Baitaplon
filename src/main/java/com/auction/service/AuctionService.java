b5f28a051696c186dcff91a6f7adcb26ea6ee0c1
ntd8386 <25021714@vnu.edu.vn>
2026-04-09 18:58:12 +0700

    Update AuctionService.java

--- src/main/java/com/auction/service/AuctionService.java
+++ src/main/java/com/auction/service/AuctionService.java
@@ -1,3 +1,24 @@
 // createAuction()
 // closeAuction()
 // show winner or something
+package com.auction.service;
+
+import com.auction.model.*;
+import java.util.*;
+
+public class AuctionService {
+    private BidService bidService = new BidService();
+    public void endAuction(Auction auction) {
+        auction.setStatus(AuctionStatus.FINISHED);
+
+        Bid winner = bidService.getWinner(auction);
+
+        if (winner != null) {
+            System.out.println("Winner: " + winner.getBidder().getName());
+
+            Bidder b = winner.getBidder();
+            b.setBalance(b.getBalance() - winner.getAmount());
+        }
+    }
+
+}
