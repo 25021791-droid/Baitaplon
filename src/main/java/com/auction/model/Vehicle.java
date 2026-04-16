2305fad9931e412777060180a339c363923a507c
25021791-droid <25021791@vnu.edu.vn>
2026-04-16 11:54:55 +0700

    Rename parameter 'item' to 'name' in Vehicle constructor

--- src/main/java/com/auction/model/Vehicle.java
+++ src/main/java/com/auction/model/Vehicle.java
@@ -1,6 +1,6 @@
 package com.auction.model;
 public class Vehicle extends Item {
-    public Vehicle(int id, String item) {
-        super(id, item);
+    public Vehicle(int id, String name) {
+        super(id, name);
     }
 }
