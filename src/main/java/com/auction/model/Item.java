77269102c3b3442252b086b9961edd5d45d1288f
25021791-droid <25021791@vnu.edu.vn>
2026-04-16 11:46:00 +0700

    Add id field to Item class and update constructor

--- src/main/java/com/auction/model/Item.java
+++ src/main/java/com/auction/model/Item.java
@@ -4,15 +4,17 @@ import java.util.List;
 import java.util.ArrayList;
 
 public abstract class Item {
+    protected int id;
     protected String item;
     protected List<Bid> bidHistory;
 
-    public Item(String item) {
+    public Item(int id, String item) {
+        this.id = id;
         this.item = item;
         this.bidHistory = new ArrayList<>();
     }
     public int getId() {
-        return this.userId;
+        return this.id;
     }
     public String getItem() {
         return this.item;
