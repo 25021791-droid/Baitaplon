abc7c5dc7e01cda618fba6ac5149691b7c9bcd11
25021791-droid <25021791@vnu.edu.vn>
2026-04-16 11:47:11 +0700

    Update Electronics.java

--- src/main/java/com/auction/model/Electronics.java
+++ src/main/java/com/auction/model/Electronics.java
@@ -1,6 +1,6 @@
 package com.auction.model;
 public class Electronics extends Item {
-    public Electronics(String item) {
-        super(item);
+    public Electronics(int id, String item) {
+        super(id, item);
     }
 }
