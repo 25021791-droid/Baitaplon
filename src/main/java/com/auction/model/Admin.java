780095f514696ef098656fef4306728aca81c18e
25021791-droid <25021791@vnu.edu.vn>
2026-04-16 11:23:50 +0700

    Rename constructor parameter from id to userId

--- src/main/java/com/auction/model/Admin.java
+++ src/main/java/com/auction/model/Admin.java
@@ -2,8 +2,8 @@ package com.auction.model;
 
 public class Admin extends User {
 
-    public Admin(int id, String username, String password, String email) {
-        super(id, username, password, email);
+    public Admin(int userId, String username, String password, String email) {
+        super(userId, username, password, email);
     }
 
     public void manageUsers() {
@@ -13,7 +13,11 @@ public class Admin extends User {
     public void manageAuctions() {
         System.out.println("Managing auctions...");
     }
-
+    
+    public void resolveDispute() {
+        System.out.println("Resolving disputes...");
+    }
+ 
     @Override
     public void displayDashboard() {
         System.out.println("Admin Dashboard");
