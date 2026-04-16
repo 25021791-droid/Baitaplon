1ec726fa033d876b8e517d92a565adf74973d312
25021791-droid <25021791@vnu.edu.vn>
2026-04-16 12:02:17 +0700

    Rename myItem to myItems in Seller class

--- src/main/java/com/auction/model/Seller.java
+++ src/main/java/com/auction/model/Seller.java
@@ -4,30 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Seller extends User {
-    private List<Item> myItem;
+    private List<Item> myItems;
 
     public Seller(int userId, String userName, String password, String email) {
         super(userId, userName, password, email);
-        this.myItem = new ArrayList<>();
+        this.myItems = new ArrayList<>();
     }
 
     public void addItem(Item item) {
-        myItem.add(item);
+        myItems.add(item);
     }
     public void editItem(Item item) {
-        for (int i = 0; i < myItem.size(); i++) {
-            if (myItem.get(i).getId() == item.getId()) {
-                myItem.set(i, item);
+        for (int i = 0; i < myItems.size(); i++) {
+            if (myItems.get(i).getId() == item.getId()) {
+                myItems.set(i, item);
                 System.out.println("Item updated");
                 return;
             }
         }
     }
     public void removeItem(Item item) {
-        myItem.remove(item);
+        myItems.remove(item);
     }
     public List<Item> getmyItem() {
-        return myItem;
+        return myItems;
     }
     @Override
     public void displayDashboard() {
