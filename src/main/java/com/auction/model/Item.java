412928e08b37707aa108b749c64bb166dc1c1a51
25021791-droid <25021791@vnu.edu.vn>
2026-04-16 11:54:34 +0700

    Rename item property to name in Item class

--- src/main/java/com/auction/model/Item.java
+++ src/main/java/com/auction/model/Item.java
@@ -5,19 +5,19 @@ import java.util.ArrayList;
 
 public abstract class Item {
     protected int id;
-    protected String item;
+    protected String name;
     protected List<Bid> bidHistory;
 
-    public Item(int id, String item) {
+    public Item(int id, String name) {
         this.id = id;
-        this.item = item;
+        this.name = name;
         this.bidHistory = new ArrayList<>();
     }
     public int getId() {
         return this.id;
     }
     public String getItem() {
-        return this.item;
+        return this.name;
     }
     public void addBid(Bid newBid) {
         this.bidHistory.add(newBid);
