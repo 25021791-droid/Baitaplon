1b0c58b6c6e70419af58574f932510117840890e
25021791-droid <25021791@vnu.edu.vn>
2026-04-16 11:32:40 +0700

    Refactor Art constructor to accept a single item string

--- src/main/java/com/auction/model/Art.java
+++ src/main/java/com/auction/model/Art.java
@@ -1,6 +1,6 @@
 package com.auction.model;
 public class Art extends Item{
-    public Art(int id, String name) {
-        super(id, name);
+    public Art(String item) {
+        super(item);
     }
 }
